3ae0a96b5142d9be6cc65360c98106744620f27e
DongHun, Won <150761282+gdevhun@users.noreply.github.com>
2024-09-26 21:57:11 +0900

    [Silver III] Title: 수열, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,36 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+int n, k, temp, psum[100001], ret = -1000000;
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int N, K;
-    cin >> N >> K;
-    
-    long long day[100001];
-    for (int i = 0; i < N; i++) {
-        cin >> day[i];
-    }
-
-    long long max_sum = 0;
-    long long current_sum = 0;
-
-    // 처음 K일의 온도 합을 계산
-    for (int i = 0; i < K; i++) {
-        current_sum += day[i];
-    }
-    
-    max_sum = current_sum;
-
-    // 슬라이딩 윈도우
-    for (int i = K; i < N; i++) {
-        current_sum += day[i] - day[i - K]; // 현재 합에서 가장 오래된 온도를 빼고 새 온도를 더함
-        if (current_sum > max_sum) {
-            max_sum = current_sum;
-        }
-    }
-
-    cout << max_sum << "\n";
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		cin >> temp;
+		psum[i] = psum[i-1] + temp;
+	}
+	
+	for (int i = k; i <= n; i++) {
+		ret = max(ret, psum[i] - psum[i - k]);
+	}
+	cout << ret << "\n";
 }
