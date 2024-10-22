ed8643a989357bbbda6a1d61762a698474ea3d4a
DongHun, Won <150761282+gdevhun@users.noreply.github.com>
2024-10-22 14:11:39 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.cc
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -14,19 +14,16 @@ int main() {
 		}
 	}
 
-	// 구름이 오른쪽으로 몇 분 후에 도달할지 계산
 	for (int i = 0; i < h; i++) {
 		int time = -1;
 		for (int j = 0; j < w; j++) {
-			if (sky[i][j] == 0) {
-				time = 0;  // 구름이 있는 구역을 발견하면 시간 0
-			}
+			if (sky[i][j] == 0) time = 0;
 			else if (time != -1) {
-				time++;  // 구름이 있는 구역 이후로 1분씩 증가
-				sky[i][j] = time;
+				sky[i][j] = ++time;
 			}
 		}
 	}
+	
 
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < w; j++) {
