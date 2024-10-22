ea86538a1e8c86df466a7a16b3809e6770807f84
DongHun, Won <150761282+gdevhun@users.noreply.github.com>
2024-10-22 15:04:44 +0900

    [Silver III] Title: 교수가 된 현우, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -1,6 +1,5 @@
 #include <iostream>
-using namespace std;
-typedef unsigned long long ll;      
+using namespace std;      
 int n, a;
 int main () {
 	ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
