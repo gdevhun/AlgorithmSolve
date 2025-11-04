3cdfb7af2230646879170a7f7c361ac51117caba
DongHun, Won <wjh9330@naver.com>
2025-11-04 16:13:24 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,32 +1,33 @@
-#include <iostream>
+
+#include<iostream>
+#include <string>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int parkingTime[101];
-int totalcost;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    //트럭 3대 총 비용 출력
-    //한대 1분에 1대당 A원 
-    //두대 1분에 1대당 B원
-    //세대 1분에 1대당 C원
+	//1분 1대당 A원, 1분 2대당 B원, 1분 3대당 C원
+	vector<int> vec(101);
+	int A, B, C;
+	cin >> A >> B >> C;
 
-    //A,B,C 주어짐 트럭 주차장에 주차된 시간 총 주차비용?
-    int first, second;
-    int A, B, C;
-    cin >> A >> B >> C;
-    for (int i = 0; i < 3; i++) {
-        cin >> first >> second;
-        for (int j = first; j < second; j++) parkingTime[j]++;
-    }
-    for (int i = 1; i < 101; i++) {
-        if (parkingTime[i] == 1) totalcost += A;
-        else if (parkingTime[i] == 2) totalcost += B * 2;
-        else if (parkingTime[i] == 3) totalcost += C * 3;
-    }
-    cout << totalcost << '\n';
-
+	for (int i=0; i<3; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		for (int j=a; j<b; j++)
+		{
+			vec[j]++;
+		}
+	}
+	int res = 0;
+	for (int i=0; i<101; i++)
+	{
+		if (vec[i] == 1) res += vec[i] * A;
+		else if (vec[i] == 2) res += vec[i] * B;
+		else if (vec[i] == 3) res += vec[i] * C;
+	}
+	cout << res;
+	return 0;
 }
