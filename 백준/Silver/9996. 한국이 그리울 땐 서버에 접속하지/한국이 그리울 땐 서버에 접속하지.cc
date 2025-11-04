f742662eb132d5bd059926b5cb9ff67bef83a4da
DongHun, Won <wjh9330@naver.com>
2025-11-04 21:20:47 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,28 +1,32 @@
-#include <iostream>
-#include <algorithm>
+#include<iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int n; string pattern, inputstr;
-    cin >> n >> pattern;
-
-    int star_idx = pattern.find("*");
-    string prefix = pattern.substr(0, star_idx);
-    string suffix = pattern.substr(star_idx + 1);
-
-    for (int i = 0; i < n; i++) {
-        cin >> inputstr;
-        if (prefix.size() + suffix.size() > inputstr.size()) cout << "NE" << '\n';
-        else {
-            if (prefix == inputstr.substr(0, prefix.size()) && suffix == inputstr.substr(inputstr.size() - suffix.size())) cout << "DA" << '\n';
-            else cout << "NE" << '\n';
-        }
-    }
-
-    return 0;
+int main()
+{
+	int n;
+	cin >> n;
+	string pattern;
+	cin >> pattern;
+	int star_idx = pattern.find("*");
+	string prefix = pattern.substr(0, star_idx);
+	string suffix = pattern.substr(star_idx + 1);
+	while (n--)
+	{
+		string input;
+		cin >> input;
+		if (input.size() < prefix.size() + suffix.size()) cout << "NE" << "\n";
+		else
+		{
+			if (prefix == input.substr(0, prefix.size()) && suffix == input.substr(input.size() - suffix.size()))
+			{
+				cout << "DA" << "\n";
+			}
+			else cout << "NE" << "\n";
+		}
+	}
+	
+	return 0;
 }
