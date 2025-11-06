e57ed36e6ba25713b2e8c0bd3329edeb9d28ce00
DongHun, Won <wjh9330@naver.com>
2025-11-06 17:54:46 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.cc
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -1,23 +1,34 @@
 #include<iostream>
+#include <string>
+#include <vector>
+int canvas[101][101];
 using namespace std;
-int arr[105][105] = { 0, };
-int main() {
-	int total;
-	cin >> total;
-	for (int i = 0; i < total; i++) {
-		int a, b;
-		cin >> a >> b;
-		for (int j = 0; j < 10; j++) {
-			for (int k = 0; k < 10; k++) {
-				arr[a + j][b + k] = 1;
+int main()
+{
+	int papernum;
+	int area = 0;
+	cin >> papernum;
+	for (int i=0; i<papernum; i++)
+	{
+		int x, y;
+		cin >> x >> y; //x=3 y=7
+
+		for (int i=y; i<y+10; i++)
+		{
+			for (int j=x; j<x+10; j++)
+			{
+				canvas[i][j] = 1;
 			}
 		}
 	}
-	int area = 0;
-	for (int i = 0; i < 101; i++) {
-		for (int j = 0; j < 101; j++) {
-			if (arr[i][j] == 1) area++;
+
+	for (int i=0; i<101; i++)
+	{
+		for (int j=0; j<101; j++)
+		{
+			if (canvas[i][j] == 1) area++;
 		}
 	}
-	cout << area << endl;
+	cout << area;
 }
+	
