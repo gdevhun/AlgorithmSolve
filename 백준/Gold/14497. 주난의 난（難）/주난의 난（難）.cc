70d3eb8a75708262c32b75ebfa1f660d17c16c5a
DongHun, Won <wjh9330@naver.com>
2025-12-04 21:51:57 +0900

    [Gold IV] Title: 주난의 난(難), Time: 52 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
@@ -1,57 +1,63 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 int n, m;
 int sx, sy, tx, ty;
-char mp[301][301];
+char map[301][301];
 bool visited[301][301];
 
-int dy[4] = {0,0,1,-1};
-int dx[4] = {1,-1,0,0};
+int dy[4] = { 0,0,1,-1 };
+int dx[4] = { 1,-1,0,0 };
 
 int bfs(int sy, int sx)
 {
-    queue<pair<int,int>> q, nextq;
+    queue<pair<int, int>> q, nextq;
     int jump = 0;
 
-    q.push({sy, sx});
+    q.push({ sy, sx });
 
     while (1)
     {
         jump++;
 
-        memset(visited, 0, sizeof(visited));
+        fill(&visited[0][0], &visited[0][0] + 301 * 301, 0);
+        //memset(visited, 0, sizeof(visited));
 
         while (!q.empty())
         {
-            auto [y, x] = q.front();
+            int cy = q.front().first;
+            int cx = q.front().second;
+  
             q.pop();
 
-            for (int i=0; i<4; i++)
+            for (int i = 0; i < 4; i++)
             {
-                int ny = y + dy[i];
-                int nx = x + dx[i];
+                int ny = cy + dy[i];
+                int nx = cx + dx[i];
 
                 if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
                 if (visited[ny][nx]) continue;
                 visited[ny][nx] = 1;
 
                 // 목표 도착
-                if (mp[ny][nx] == '#') 
+                if (map[ny][nx] == '#')
                     return jump;
 
-                if (mp[ny][nx] == '0') {
-                    q.push({ny,nx});
+                if (map[ny][nx] == '0') {
+                    q.push({ ny,nx });
                 }
-                else if (mp[ny][nx] == '1') {
-                    nextq.push({ny,nx});
+                else if (map[ny][nx] == '1') {
+                    nextq.push({ ny,nx });
                 }
             }
         }
 
         // 점프 후 이동 가능한 큐로 교체
         q = nextq;
-        nextq = queue<pair<int,int>>();
+        nextq = queue<pair<int, int>>();
     }
 }
 int main() {
@@ -62,9 +68,13 @@ int main() {
     cin >> sy >> sx >> ty >> tx;
     sy--, sx--, ty--, tx--;
 
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++)
-            cin >> mp[i][j];
+    for (int i = 0; i < n; i++) {
+        string input;
+    	cin >> input;
+        for (int j = 0; j < m; j++)
+        {
+            map[i][j] = input[j];
+        }
     }
 
     cout << bfs(sy, sx);
