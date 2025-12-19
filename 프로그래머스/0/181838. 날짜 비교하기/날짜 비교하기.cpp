7c070652f21373f8c9b3902b3ed9af8ebc09dd84
DongHun, Won <wjh9330@naver.com>
2025-12-19 14:17:18 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 3.62 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
@@ -4,16 +4,6 @@
 using namespace std;
 
 int solution(vector<int> date1, vector<int> date2) {
-    if(date1[0]<date2[0]) return 1;
-    else if(date1[0]==date2[0]) {
-        if(date1[1]<date2[1]) return 1;
-        else if(date1[1]==date2[1]){
-            if(date1[2]<date2[2]) return 1;
-            else if(date1[2]==date2[2]) return 0;
-            else return 0;
-        }
-        else return 0;
-    }
-    else return 0;
-    
+    int answer = 0;
+    return answer;
 }
